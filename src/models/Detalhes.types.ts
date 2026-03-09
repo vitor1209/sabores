a3fcf43095ce45b5c50b24dd941cc9eb97c5d823
vitor1209 <vlopes@tecnologiaunica.com.br>
2026-03-08 22:55:46 -0300

    feat(models): movies

---
+++ src/models/Detalhes.types.ts
@@ -0,0 +1,30 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace Movie {
+    export type request = {
+        name: string;
+    }
+
+    export type Movie = {
+        adult: boolean;
+        backdrop_path?: string | null;
+        genre_ids: number[];
+        id: number;
+        original_language: string;
+        original_title: string;
+        overview?: string;
+        popularity: number;
+        poster_path?: string | null;
+        release_date?: string; 
+        title: string;
+        video: boolean;
+        vote_average: number;
+        vote_count: number;
+    }
+
+    export type response = {
+        page: number;
+        results: Movie[];
+        total_pages: number;
+        total_results: number;
+    }
+}
